5810a6861c8c2fa9ed30bb28eafa01d051f1284c
kaavya <kaavyarao05@gmail.com>
2024-10-13 19:08:36 +0530

    feat: Multiple Playlists

--- Play.py
+++ Play.py
@@ -6,7 +6,7 @@ import os
 from enum import Enum
 
 class buttonState(Enum):
-    IDLE=0,  #pass
+    IDLE=0,        #play songs
     TOCLICKPREV=1  #can set the first song
     TOCLICKPOST=2  #can set the song to play after clickedSong
 curState:buttonState
@@ -17,11 +17,9 @@ class Song:
     prev:Self
     songName:str
     path:str
-    def __init__(self,root,txt,path):
+    def __init__(self,txt,path):
         self.songName=txt
         self.path=path
-        self.button=Button(root,text=txt,command=self.clicked)
-        fitInGrid(self.button)
         self.prev=None
         self.next=None
     def clicked(self):
@@ -34,11 +32,20 @@ class Song:
                 changeState(buttonState.TOCLICKPOST,self)
             case buttonState.TOCLICKPOST:
                 changeState(buttonState.TOCLICKPREV,self)
+    def setButton(self):
+        self.button=Button(canvas,text=self.songName,command=self.clicked)
+        fitInGrid(self.button)
+    def destroyButton(self):
+        self.button.destroy()
+        self.button=None
 
 class PlayList:
-    PlaylistName:str
-    path:str
-    start:Song
+    PlaylistName:str=""
+    path:str=""
+    start:Song=None
+    def __init__(self,path,name):
+        self.path=path
+        self.PlaylistName=name
 
 def set_vol(val):
     pygame.mixer.music.set_volume(float(val)/100)
@@ -47,6 +54,7 @@ def setNext(song:Song,next:Song):
     song.next=next
     next.prev=song
 
+clickedSong:Song
 def changeState(newstate:buttonState,newsong:Song=None):
     global curState
     curState=newstate
@@ -69,8 +77,6 @@ def changeState(newstate:buttonState,newsong:Song=None):
 def updateLabel(newtext:str):
     label.config(text=newtext)
 
-clickedSong:Song
-
 root =Tk()
 root.title('Music Player')
 root.geometry("500x360")
@@ -80,52 +86,83 @@ pygame.mixer.init()
 menubar=Menu(root)
 root.config(menu=menubar)
 
-start:Song=None
-songs=[]
-currentsong:Song
+playlists:list[PlayList]=[]
+currentplaylist:PlayList=None
+currentsong:Song=None
 paused=False
 
 gridrow=0
 gridcol=0
+
 def fitInGrid(songButton:Button):
     global gridcol,gridrow
     songButton.grid(row=gridrow)
     gridrow+=1
 
-def createNode(path,name):
-    global start
-    if start:
-        new=Song(canvas,name,path)
-        ptr=start
+def createNode(path,name,playlist):
+    if playlist.start:
+        new=Song(name,path)
+        ptr=playlist.start
         while ptr.next!=None:
             ptr.next.prev=ptr
             ptr=ptr.next
         ptr.next=new
         new.prev=ptr
+        new.setButton()
     else:
-        start=Song(canvas,name,path)
+        playlist.start=Song(name,path)
+        playlist.start.setButton()
 
-def load():
-    global currentsong
-    root.directory=filedialog.askdirectory()
-    for song in os.listdir(root.directory):
-        name,ext=os.path.splitext(song)
-        if ext==".mp3":
-            createNode(song,name)
-    currentsong=start
+def clearplaylist():
+    if currentplaylist:
+        ptr:Song=currentplaylist.start
+        while ptr!=None:
+            ptr.destroyButton()
+            ptr=ptr.next
+
+def changePlaylist(newplaylist:PlayList):
+    global currentplaylist
+    clearplaylist()
+    currentplaylist=newplaylist
+    ptr:Song=currentplaylist.start
+    while ptr!=None:
+        ptr.setButton()
+        ptr=ptr.next
+
+
+def load_playlist():
+    global currentsong,currentplaylist
+    dir=filedialog.askdirectory()
+    if dir:
+        clearplaylist()
+        playlist=PlayList(dir,"Playlist{}".format(len(playlists)))
+        playlists.append(playlist)
+        currentplaylist=playlist
+        
+        for songpath in os.listdir(playlist.path):
+            name,ext=os.path.splitext(songpath)
+            if ext==".mp3":
+                createNode(songpath,name,playlist)
+        currentsong=playlist.start
+        addPlaylistToMenu(playlist)
 
 def toggleEditList():
     if curState==buttonState.IDLE:
         changeState(buttonState.TOCLICKPREV)
     else:
         changeState(buttonState.IDLE)
-    
+
+def addPlaylistToMenu(playlist:PlayList):
+    playlist_menu.add_command(label=playlist.PlaylistName,command=lambda:playlistClicked(playlist))
+
+def playlistClicked(playlist:PlayList):
+    changePlaylist(playlist)
 
 def play_music():
     global currentsong,paused
     if not paused:
         updateLabel("Currently playing: {}".format(currentsong.songName))
-        pygame.mixer.music.load(os.path.join(root.directory,currentsong.path))
+        pygame.mixer.music.load_playlist(os.path.join(currentplaylist.path,currentsong.path))
         pygame.mixer.music.play()
     else:
         unpause_music()
@@ -153,9 +190,15 @@ def prev_music():
         play_music()
 
 organise_menu=Menu(menubar,tearoff=False)
-organise_menu.add_command(label="Select Folder",command=load)
+organise_menu.add_command(label="Load Playlist",command=load_playlist)
 organise_menu.add_command(label="Toggle Edit",command=toggleEditList)
+
+playlist_menu=Menu(menubar,tearoff=False)
+for playlist in playlists:
+    addPlaylistToMenu(playlist)
+
 menubar.add_cascade(label="Organise",menu=organise_menu)
+menubar.add_cascade(label="Playlists",menu=playlist_menu)
 
 label=Label(root)
 label.pack()
@@ -182,4 +225,6 @@ prev_btn.grid(row=1,column=0)
 next_btn.grid(row=1,column=3)
 
 changeState(buttonState.IDLE)
-root.mainloop()
+
+if __name__=="__main__":
+    root.mainloop()
